9163a6e32239ad0d188dc708eff2f66505a97f02
Adhnan23 <59403931+Adhnan23@users.noreply.github.com>
2024-03-01 11:13:36 +0530

    I added new variable

--- if_else.php
+++ if_else.php
@@ -3,16 +3,8 @@
 $num1 = 13;
 $num2 = 5;
 
-echo "<br>" . "num1 = $num1";
-echo "<br>" . "num2 = $num2";
-$num3 = ($num1 % $num2);
-echo "<br>" . "num3 = (num1 % num2)";
-echo "<br>" . "$num1 % $num2 =" . $num3;
-$num4 = ($num3 + $num2);
-echo "<br>" . "num4 = (num3 + num2) ";
-echo "<br>" . "$num3 + $num2 =" . $num4;
-$num5 = ($num4 ** 3);
-echo "<br>" . "num5 = (num4 ** 3) ";
-echo "<br>" . "$num4 ** 3 =" . $num5;
+$num3 = 47;
+
+echo $num1 + $num2 + $num3;
 
 ?>
